f310a9340a8ddc31be86fcd0d135dd3f97eac721
JoelFaldin <joelfaldin@gmail.com>
2026-04-02 00:54:03 -0300

    feat: add qr code to main page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { QRCodeSVG } from "qrcode.react";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-	return <h1>test</h1>;
+	return (
+		<section className="flex items-center justify-center h-screen">
+			<QRCodeSVG value="http://192.168.1.94:3000/" size={600} />
+		</section>
+	);
 }
